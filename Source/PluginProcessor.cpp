96af61569e35aaa705863df0a90a9fa55a22bd4e
duprasmartin <47566268+duprasmartin@users.noreply.github.com>
2019-04-16 16:05:17 +0100

    Add files via upload
    yo

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,213 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+NewerGainWithSliderAudioProcessor::NewerGainWithSliderAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+/*AudioParameterFloat (String parameterID,
+String name,
+float minValue,
+float maxValue,
+float defaultValue);
+*/
+{
+        addParameter(mGainParameter   =  new AudioParameterFloat("gain",
+                                                                 "gain",
+                                                                             0.0f,
+                                                                             1.0f,
+                                                                             0.5f));
+}
+
+
+
+NewerGainWithSliderAudioProcessor::~NewerGainWithSliderAudioProcessor()
+{
+}
+
+//==============================================================================
+const String NewerGainWithSliderAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool NewerGainWithSliderAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool NewerGainWithSliderAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool NewerGainWithSliderAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double NewerGainWithSliderAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int NewerGainWithSliderAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int NewerGainWithSliderAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void NewerGainWithSliderAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String NewerGainWithSliderAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void NewerGainWithSliderAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void NewerGainWithSliderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void NewerGainWithSliderAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool NewerGainWithSliderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void NewerGainWithSliderAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    
+        auto* channelLeft = buffer.getWritePointer (0); // acquire left chan samples
+        auto* channelRight = buffer.getWritePointer (1);   // ... and right
+    
+    for (auto sample = 0; sample < buffer.getNumSamples(); sample++) {
+        
+        mGainSmoothed = mGainSmoothed - mRampSpeed * (mGainSmoothed - mGainParameter->get());
+        channelLeft[sample] *= mGainSmoothed;
+        channelRight[sample] *= mGainSmoothed;
+
+        
+    }
+
+        // ..do something to the data...
+    
+}
+
+//==============================================================================
+bool NewerGainWithSliderAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* NewerGainWithSliderAudioProcessor::createEditor()
+{
+    return new NewerGainWithSliderAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void NewerGainWithSliderAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void NewerGainWithSliderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new NewerGainWithSliderAudioProcessor();
+}
